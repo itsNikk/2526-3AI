4e63aefa644086a992f65ab67315a8b399cbda82
Nicolò Buganza <56979355+itsNikk@users.noreply.github.com>
2026-03-18 13:21:05 +0100

    Update arrays.sh

--- arrays.sh
+++ arrays.sh
@@ -2,13 +2,35 @@
 
 #array = strutture dati
 # in java: omogenei, statici
-
+clear
 arr=(1 2 3 4 5 6)
 # DEVO usare le graffe
 echo ${arr[@]} #Tutti gli elementi
 echo "Length: ${#arr[@]}"
 echo ${arr[0]}
 echo ${arr[-1]}
+
+# For.. each
+for n in ${arr[@]}
+do
+    echo -n "$n "
+done
+#IFS= Internal File Separator
+for index in $(seq 0 ${#arr[@]}); do
+    echo ${arr[index]}
+done
+
+
+#SLICING - ${variabile:start:howMany}
+echo Slice1: ${arr[@]:1:4}
+echo Slice2: ${arr[@]:1:19}
+
+s="stringa"
+echo $s
+echo ${s:0:1}
+#echo ${s[0]}
+echo ${s:2:4}
+
 
 
 
