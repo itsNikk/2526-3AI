4a19b9a3069ddca13b188dafe46e4cbd7516658a
Nicolò Buganza <56979355+itsNikk@users.noreply.github.com>
2026-03-18 12:46:17 +0100

    Create arrays.sh

---
+++ arrays.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#array = strutture dati
+# in java: omogenei, statici
+
+arr=(1 2 3 4 5 6)
+# DEVO usare le graffe
+echo ${arr[@]} #Tutti gli elementi
+echo "Length: ${#arr[@]}"
+echo ${arr[0]}
+echo ${arr[-1]}
+
+
+
+
